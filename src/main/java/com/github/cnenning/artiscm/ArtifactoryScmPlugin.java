e5fd5279afe3496f371412c40f7ac8c9b879c5ac
cnenning <cnenning@apache.org>
2016-12-06 14:31:11 +0100

    added plugin settings (http timeouts and proxy, see GH #6)

--- src/main/java/com/github/cnenning/artiscm/ArtifactoryScmPlugin.java
+++ src/main/java/com/github/cnenning/artiscm/ArtifactoryScmPlugin.java
@@ -8,12 +8,14 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpHost;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.config.RequestConfig.Builder;
@@ -30,8 +32,11 @@ import com.thoughtworks.go.plugin.api.GoPluginIdentifier;
 import com.thoughtworks.go.plugin.api.annotation.Extension;
 import com.thoughtworks.go.plugin.api.exceptions.UnhandledRequestTypeException;
 import com.thoughtworks.go.plugin.api.logging.Logger;
+import com.thoughtworks.go.plugin.api.request.DefaultGoApiRequest;
+import com.thoughtworks.go.plugin.api.request.GoApiRequest;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
 import com.thoughtworks.go.plugin.api.response.DefaultGoPluginApiResponse;
+import com.thoughtworks.go.plugin.api.response.GoApiResponse;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 
 @Extension
@@ -39,6 +44,13 @@ public class ArtifactoryScmPlugin implements GoPlugin {
 
 	public static final String EXTENSION_NAME = "scm";
 	private static final List<String> GO_SUPPORTED_VERSIONS = Arrays.asList("1.0");
+	private static final GoPluginIdentifier GO_PLUGIN_ID = new GoPluginIdentifier(EXTENSION_NAME, GO_SUPPORTED_VERSIONS);
+	private static final String GO_API_VERSION = "1.0";
+
+	public static final String REQUEST_SETTINGS_GET_CONFIG = "go.plugin-settings.get-configuration";
+	public static final String REQUEST_SETTINGS_GET_VIEW = "go.plugin-settings.get-view";
+	public static final String REQUEST_SETTINGS_VALIDATE = "go.plugin-settings.validate-configuration";
+	public static final String REQUEST_SETTINGS_GET_THEM = "go.processor.plugin-settings.get";
 
 	public static final String REQUEST_SCM_CONFIGURATION = "scm-configuration";
 	public static final String REQUEST_SCM_VIEW = "scm-view";
@@ -50,7 +62,7 @@ public class ArtifactoryScmPlugin implements GoPlugin {
 
 	public static final DateFormat GO_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
 
-	public static final int DEFAULT_TIMEOUT = 30;
+	private static final int DEFAULT_TIMEOUT = 240;
 
 	private Logger logger = Logger.getLoggerFor(getClass());
 
@@ -58,23 +70,66 @@ public class ArtifactoryScmPlugin implements GoPlugin {
 
 	public ArtifactoryScmPlugin() {
 		logger.debug("plugin instance created");
-
-		Builder requestConfigBuilder = RequestConfig.custom();
-		requestConfigBuilder.setConnectTimeout(DEFAULT_TIMEOUT);
-		
-		HttpClientBuilder clientBuilder = HttpClientBuilder.create();
-		clientBuilder.setDefaultRequestConfig(requestConfigBuilder.build());
-		httpClient = clientBuilder.build();
 	}
 
 	@Override
 	public GoPluginIdentifier pluginIdentifier() {
-		return new GoPluginIdentifier(EXTENSION_NAME, GO_SUPPORTED_VERSIONS);
+		return GO_PLUGIN_ID;
 	}
 
 	@Override
 	public void initializeGoApplicationAccessor(GoApplicationAccessor goApplicationAccessor) {
-		// ignore
+		logger.debug("initializeGoApplicationAccessor()");
+
+		GoApiRequest request = new DefaultGoApiRequest(REQUEST_SETTINGS_GET_THEM, GO_API_VERSION, GO_PLUGIN_ID);
+		GoApiResponse response = goApplicationAccessor.submit(request);
+
+		String json = response.responseBody();
+
+		httpClient = createHttpClient(json);
+	}
+
+	private HttpClient createHttpClient(String json) {
+		Builder requestConfigBuilder = RequestConfig.custom();
+		try {
+			Map settings = json != null
+				? new ObjectMapper().readValue(json, Map.class)
+				: Collections.EMPTY_MAP;
+
+			int timeout = timeoutFromSettings(settings, "connectTimeout");
+			logger.info("setting connectTimeout: " + timeout);
+			requestConfigBuilder.setConnectTimeout(timeout);
+
+			timeout = timeoutFromSettings(settings, "socketTimeout");
+			logger.info("setting socketTimeout: " + timeout);
+			requestConfigBuilder.setSocketTimeout(timeout);
+
+			timeout = timeoutFromSettings(settings, "connectionRequestTimeout");
+			logger.info("setting connectionRequestTimeout: " + timeout);
+			requestConfigBuilder.setConnectionRequestTimeout(timeout);
+
+			String proxyUrl = (String)settings.get("proxyUrl");
+			if (proxyUrl != null && !proxyUrl.isEmpty()) {
+				logger.info("setting proxyUrl: " + proxyUrl);
+				requestConfigBuilder.setProxy(HttpHost.create(proxyUrl));
+			}
+		} catch (Exception e) {
+			logger.error("could not read plugin settings", e);
+		}
+
+		HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+		clientBuilder.setDefaultRequestConfig(requestConfigBuilder.build());
+		return clientBuilder.build();
+	}
+
+	private int timeoutFromSettings(Map settings, String key) {
+		int timeout = DEFAULT_TIMEOUT;
+		try {
+			timeout = Integer.parseInt((String)settings.get(key));
+		} catch (Exception e) {
+			// ignore
+		}
+		return timeout;
 	}
 
 	@Override
@@ -84,7 +139,13 @@ public class ArtifactoryScmPlugin implements GoPlugin {
 		String body = requestMessage.requestBody();
 		logger.debug("got requestName: '" + name + "', with body: " + body);
 		try {
-			if (REQUEST_SCM_CONFIGURATION.equals(name)) {
+			if (REQUEST_SETTINGS_GET_CONFIG.equals(name)) {
+				return toJson(handlePluginConfig());
+			} else if (REQUEST_SETTINGS_GET_VIEW.equals(name)) {
+				return toJson(handlePluginConfigView());
+			} else if (REQUEST_SETTINGS_VALIDATE.equals(name)) {
+				return toJson(handlePluginConfigValidation(body));
+			} else if (REQUEST_SCM_CONFIGURATION.equals(name)) {
 				return toJson(handleScmConfig());
 			} else if (REQUEST_SCM_VIEW.equals(name)) {
 				return toJson(handleScmView());
@@ -106,6 +167,113 @@ public class ArtifactoryScmPlugin implements GoPlugin {
 			logger.debug("operation took: " + (endTime - startTime) + " ms");
 		}
 		return null;
+	}
+
+	private Map<String, Object> handlePluginConfig() {
+		Map<String, Object> wrapper = new HashMap<>();
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("display-name", "Connect Timeout");
+		map.put("default-value", String.valueOf(DEFAULT_TIMEOUT));
+		map.put("display-order", "0");
+		map.put("required", Boolean.FALSE);
+		map.put("secure", Boolean.FALSE);
+		wrapper.put("connectTimeout", map);
+
+		map = new HashMap<>();
+		map.put("display-name", "Socket Timeout");
+		map.put("default-value", String.valueOf(DEFAULT_TIMEOUT));
+		map.put("display-order", "1");
+		map.put("required", Boolean.FALSE);
+		map.put("secure", Boolean.FALSE);
+		wrapper.put("socketTimeout", map);
+
+		map = new HashMap<>();
+		map.put("display-name", "Connection Request Timeout");
+		map.put("default-value", String.valueOf(DEFAULT_TIMEOUT));
+		map.put("display-order", "2");
+		map.put("required", Boolean.FALSE);
+		map.put("secure", Boolean.FALSE);
+		wrapper.put("connectionRequestTimeout", map);
+
+		map = new HashMap<>();
+		map.put("display-name", "Proxy URL");
+		map.put("default-value", "");
+		map.put("display-order", "3");
+		map.put("required", Boolean.FALSE);
+		map.put("secure", Boolean.FALSE);
+		wrapper.put("proxyUrl", map);
+
+		return wrapper;
+	}
+
+	private Map<String, String> handlePluginConfigView() throws IOException {
+		InputStream inputStream = getClass().getResourceAsStream("/plugin-config.html");
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		IOUtils.copy(inputStream, baos);
+		String view = baos.toString();
+
+		Map<String, String> map = new HashMap<>();
+		map.put("template", view);
+		return map;
+	}
+
+	private List<Object> handlePluginConfigValidation(String inputJson) throws JsonParseException, JsonMappingException, IOException {
+		Map config = new ObjectMapper().readValue(inputJson, Map.class);
+		List<Object> valiErrors = new ArrayList<>();
+
+		config = (Map)config.get("plugin-settings");
+
+		validateTimeout(valiErrors, config, "connectTimeout");
+		validateTimeout(valiErrors, config, "socketTimeout");
+		validateTimeout(valiErrors, config, "connectionRequestTimeout");
+
+		Map proxyMap = (Map)config.get("proxyUrl");
+		if (proxyMap != null) {
+			String proxy = (String)proxyMap.get("value");
+			try {
+				HttpHost.create(proxy);
+			} catch (Exception e) {
+				Map<String, String> error = new HashMap<>();
+				error.put("key", "proxyUrl");
+				error.put("message", e.toString());
+				valiErrors.add(error);
+				
+			}
+		}
+
+		return valiErrors;
+	}
+
+	private void validateTimeout(List<Object> valiErrors, Map config, String key) {
+		String value = null;
+		Map valueMap = (Map)config.get(key);
+		if (valueMap != null) {
+			value = (String)valueMap.get("value");
+		}
+		logger.debug("validating '" + key + "' = '" + value + "'");
+		if (!validateTimeoutValue(value)) {
+			Map<String, String> error = new HashMap<>();
+			error.put("key", key);
+			error.put("message", "Invalid timeout value. Must be an integer > 0 or -1.");
+			valiErrors.add(error);
+		}
+	}
+
+	private boolean validateTimeoutValue(String val) {
+		if (val == null || val.isEmpty()) {
+			return true;
+		}
+		try {
+			int timeout = Integer.parseInt(val);
+			if (timeout > 0 || timeout == -1) {
+				return true;
+			}
+		} catch (Exception e) {
+			// just log in debug
+			logger.debug("could not parse timeout value: " + val, e);
+		}
+		return false;
 	}
 
 	private Map<String, Object> handleScmConfig() {
@@ -138,7 +306,7 @@ public class ArtifactoryScmPlugin implements GoPlugin {
 		String url = urlFromConfig(config);
 		logger.debug("validating url: " + url);
 
-		List<String> validationMessages = doValidation(url);
+		List<String> validationMessages = doScmValidation(url);
 
 		for (String msg : validationMessages) {
 			Map<String, String> error = new HashMap<>();
@@ -158,7 +326,7 @@ public class ArtifactoryScmPlugin implements GoPlugin {
 		List<String> messages = new ArrayList<>();
 
 		// do validation first
-		List<String> validationErrors = doValidation(url);
+		List<String> validationErrors = doScmValidation(url);
 		if (!validationErrors.isEmpty()) {
 			for (String valiError : validationErrors) {
 				messages.add(valiError);
@@ -187,7 +355,7 @@ public class ArtifactoryScmPlugin implements GoPlugin {
 		return map;
 	}
 
-	private List<String> doValidation(String url) {
+	private List<String> doScmValidation(String url) {
 		List<String> valiErrors = new ArrayList<>();
 		if (url == null || url.isEmpty()) {
 			valiErrors.add("URL not specified");
--- src/test/java/com/github/cnenning/artiscm/integrationtest/IntegrationTests.java
+++ src/test/java/com/github/cnenning/artiscm/integrationtest/IntegrationTests.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 import org.apache.commons.io.IOUtils;
@@ -15,9 +16,14 @@ import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.cnenning.artiscm.ArtifactoryScmPlugin;
+import com.thoughtworks.go.plugin.api.GoApplicationAccessor;
 import com.thoughtworks.go.plugin.api.request.DefaultGoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.request.GoApiRequest;
 import com.thoughtworks.go.plugin.api.request.GoPluginApiRequest;
+import com.thoughtworks.go.plugin.api.response.DefaultGoApiResponse;
+import com.thoughtworks.go.plugin.api.response.GoApiResponse;
 import com.thoughtworks.go.plugin.api.response.GoPluginApiResponse;
 
 public class IntegrationTests {
@@ -77,10 +83,144 @@ public class IntegrationTests {
 		TMP_DIR.delete();
 	}
 
+	protected ArtifactoryScmPlugin createPlugin() {
+		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		plugin.initializeGoApplicationAccessor(new GoApplicationAccessor(){
+			
+			@Override
+			public GoApiResponse submit(GoApiRequest request)
+			{
+				return new DefaultGoApiResponse(200);
+			}
+		});
+		return plugin;
+	}
+
 	protected GoPluginApiRequest createRequest(String name, String body) {
 		DefaultGoPluginApiRequest request = new DefaultGoPluginApiRequest("scm", "1.0", name);
 		request.setRequestBody(body);
 		return request;
+	}
+
+	@Test
+	public void settingsParsing() throws Exception {
+		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		plugin.initializeGoApplicationAccessor(new GoApplicationAccessor(){
+			
+			@Override
+			public GoApiResponse submit(GoApiRequest request)
+			{
+				String json = "{"
+						+ "\"connectTimeout\": \"1\","
+						+ "\"socketTimeout\": \"2\","
+						+ "\"connectionRequestTimeout\": \"3\","
+						+ "\"proxyUrl\": \"http://proxy.example.com:1234\""
+						+ "}";
+				DefaultGoApiResponse response = new DefaultGoApiResponse(200);
+				response.setResponseBody(json);
+				return response;
+			}
+		});
+
+		// just make sure there is no exception
+	}
+
+	@Test
+	public void pluginSettings() throws Exception {
+		String requestJson =
+				"{}"
+		;
+		GoPluginApiRequest request = createRequest("go.plugin-settings.get-configuration", requestJson);
+
+		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		GoPluginApiResponse response = plugin.handle(request);
+
+		Assert.assertNotNull(response);
+		Map map = new ObjectMapper().readValue(response.responseBody(), Map.class);
+		Map connectTimeoutMap = (Map) map.get("connectTimeout");
+		Map socketTimeoutMap = (Map) map.get("socketTimeout");
+		Map connectionRequestTimeoutMap = (Map) map.get("connectionRequestTimeout");
+		Map proxyUrlMap = (Map) map.get("proxyUrl");
+
+		Assert.assertEquals("240", connectTimeoutMap.get("default-value"));
+		Assert.assertEquals("0", connectTimeoutMap.get("display-order"));
+		Assert.assertEquals(Boolean.FALSE, connectTimeoutMap.get("required"));
+		Assert.assertEquals(Boolean.FALSE, connectTimeoutMap.get("secure"));
+
+		Assert.assertEquals("240", socketTimeoutMap.get("default-value"));
+		Assert.assertEquals("1", socketTimeoutMap.get("display-order"));
+		Assert.assertEquals(Boolean.FALSE, socketTimeoutMap.get("required"));
+		Assert.assertEquals(Boolean.FALSE, socketTimeoutMap.get("secure"));
+
+		Assert.assertEquals("240", connectionRequestTimeoutMap.get("default-value"));
+		Assert.assertEquals("2", connectionRequestTimeoutMap.get("display-order"));
+		Assert.assertEquals(Boolean.FALSE, connectionRequestTimeoutMap.get("required"));
+		Assert.assertEquals(Boolean.FALSE, connectionRequestTimeoutMap.get("secure"));
+
+		Assert.assertEquals("", proxyUrlMap.get("default-value"));
+		Assert.assertEquals("3", proxyUrlMap.get("display-order"));
+		Assert.assertEquals(Boolean.FALSE, proxyUrlMap.get("required"));
+		Assert.assertEquals(Boolean.FALSE, proxyUrlMap.get("secure"));
+	}
+
+	@Test
+	public void settingsValidationOk() throws Exception {
+		String requestJson =
+				"{\"plugin-settings\": {"
+					+ "\"connectTimeout\": {"
+					+ "\"value\": \"5\""
+					+ "}, "
+					+ "\"proxyUrl\": {"
+					+ "\"value\": \"http://proxy.example.com:1234\""
+					+ "}"
+				+ "}}"
+		;
+		GoPluginApiRequest request = createRequest("go.plugin-settings.validate-configuration", requestJson);
+
+		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		GoPluginApiResponse response = plugin.handle(request);
+
+		Assert.assertNotNull(response);
+		Assert.assertTrue(response.responseBody().equals("[]"));
+	}
+
+	@Test
+	public void settingsValidationBadInteger() throws Exception {
+		String requestJson =
+				"{\"plugin-settings\": {"
+					+ "\"connectTimeout\": {"
+					+ "\"value\": \"asdf\""
+					+ "}"
+				+ "}}"
+		;
+		GoPluginApiRequest request = createRequest("go.plugin-settings.validate-configuration", requestJson);
+
+		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		GoPluginApiResponse response = plugin.handle(request);
+
+		Assert.assertNotNull(response);
+		Assert.assertTrue(response.responseBody().contains("\"key\":\"connectTimeout\""));
+		Assert.assertTrue(response.responseBody().contains("\"message\":\"Invalid timeout value. Must be an integer > 0 or -1.\""));
+	}
+
+	@Test
+	public void settingsValidationBadProxy() throws Exception {
+		String requestJson =
+				"{\"plugin-settings\": {"
+					+ "\"proxyUrl\": {"
+					+ "\"value\": \"ftp://whatever:anc\""
+					+ "}"
+				+ "}}"
+		;
+		GoPluginApiRequest request = createRequest("go.plugin-settings.validate-configuration", requestJson);
+
+		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		GoPluginApiResponse response = plugin.handle(request);
+
+		Assert.assertNotNull(response);
+		Assert.assertTrue(response.responseBody().contains("\"key\":\"proxyUrl\""));
+		Assert.assertTrue(response.responseBody().contains("\"message\":"));
+		Assert.assertTrue(response.responseBody().contains("Exception"));
 	}
 
 	@Test
@@ -98,7 +238,6 @@ public class IntegrationTests {
 		Assert.assertTrue(response.responseBody().contains("\"display-name\":\"url\""));
 		Assert.assertTrue(response.responseBody().contains("\"part-of-identity\":false"));
 	}
-
 
 	@Test
 	public void scmValidationOk() throws Exception {
@@ -186,7 +325,7 @@ public class IntegrationTests {
 		;
 		GoPluginApiRequest request = createRequest("check-scm-connection", requestJson);
 
-		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		ArtifactoryScmPlugin plugin = createPlugin();
 		GoPluginApiResponse response = plugin.handle(request);
 
 		Assert.assertNotNull(response);
@@ -204,7 +343,7 @@ public class IntegrationTests {
 		;
 		GoPluginApiRequest request = createRequest("latest-revision", requestJson);
 
-		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		ArtifactoryScmPlugin plugin = createPlugin();
 		GoPluginApiResponse response = plugin.handle(request);
 
 		Assert.assertNotNull(response);
@@ -231,7 +370,7 @@ public class IntegrationTests {
 		;
 		GoPluginApiRequest request = createRequest("latest-revision", requestJson);
 
-		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		ArtifactoryScmPlugin plugin = createPlugin();
 		GoPluginApiResponse response = plugin.handle(request);
 
 		Assert.assertNotNull(response);
@@ -262,7 +401,7 @@ public class IntegrationTests {
 		;
 		GoPluginApiRequest request = createRequest("latest-revisions-since", requestJson);
 
-		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		ArtifactoryScmPlugin plugin = createPlugin();
 		GoPluginApiResponse response = plugin.handle(request);
 
 		Assert.assertNotNull(response);
@@ -300,7 +439,7 @@ public class IntegrationTests {
 		;
 		GoPluginApiRequest request = createRequest("checkout", requestJson);
 
-		ArtifactoryScmPlugin plugin = new ArtifactoryScmPlugin();
+		ArtifactoryScmPlugin plugin = createPlugin();
 		GoPluginApiResponse response = plugin.handle(request);
 
 		Assert.assertNotNull(response);
